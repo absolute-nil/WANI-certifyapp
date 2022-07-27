4cba62291edaad5137d7f834174f3e50cc9d8685
Nikhil Sharma <nikhil.sk.sharma@gmail.com>
2022-07-27 08:42:55 +0530

    add form to verify cpurl

--- src/UserInfo.js
+++ src/UserInfo.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import axios from "axios"
 
 class UserInfo extends Component {
 
@@ -7,12 +8,30 @@ class UserInfo extends Component {
     this.state = {
       name: "",
       email: "",
-      id: ""
+      id: "",
+      cpUrl: "",
+      response: ""
     };
     this.props.keycloak.loadUserInfo().then(userInfo => {
-        this.setState({name: userInfo.name, email: userInfo.email, id: userInfo.sub})
+      this.setState({ name: userInfo.name, email: userInfo.email, id: userInfo.sub })
     });
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
   }
+
+  handleChange(event) {
+    this.setState({ cpUrl: event.target.value });
+  }
+
+  async handleSubmit(event) {
+    event.preventDefault();
+
+    const response = await axios.get(`https://wani.productnation.in/api/v1/token/pass-waniAppToken?cpUrl=${this.state.cpUrl}`)
+
+    this.setState({ response: response.data });
+  }
+
 
   render() {
     return (
@@ -20,6 +39,15 @@ class UserInfo extends Component {
         <p>Name: {this.state.name}</p>
         <p>Email: {this.state.email}</p>
         <p>ID: {this.state.id}</p>
+        <form onSubmit={this.handleSubmit}>
+          <label>
+            Cp Url:
+            <input type="text" value={this.state.value} onChange={this.handleChange} />
+          </label>
+          <input type="submit" value="Submit" />
+        </form>
+        <p>Response: {this.state.response}</p>
+
       </div>
     );
   }
