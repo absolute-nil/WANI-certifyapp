5fdb8cdd4748b918b6e7786497a4e438b201aded
Nikhil Sharma <nikhil.sk.sharma@gmail.com>
2022-08-01 03:51:23 +0530

    add conditional rendering for login and logout

--- src/components/header.js
+++ src/components/header.js
@@ -1,21 +1,33 @@
 import Container from 'react-bootstrap/Container';
 import Navbar from 'react-bootstrap/Navbar';
 import { Link } from 'react-router-dom';
+import Logout from './logout';
+import React, { Component } from 'react';
 
-function Header() {
-  return (
-    <Navbar bg="dark" variant="dark">
-      <Container>
-        <Navbar.Brand href="/">PM-WANI</Navbar.Brand>
-        <Navbar.Toggle />
-        <Navbar.Collapse className="justify-content-end">
-          <Navbar.Text>
-            <Link to="/secured">Secured </Link>
-          </Navbar.Text>
-        </Navbar.Collapse>
-      </Container>
-    </Navbar>
-  );
+class Header extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <>
+        <Navbar bg="dark" variant="dark">
+          <Container>
+            <Navbar.Brand href="/">PM-WANI</Navbar.Brand>
+            <Navbar.Toggle />
+            <Navbar.Collapse className="justify-content-end">
+              <Navbar.Text>
+                {
+                  this.props.loggedIn ? <Logout toggleAuthentication={this.props.toggleAuthentication} keycloak={this.props.keycloak} /> : <Link to="/secured">Log In </Link>
+                }
+              </Navbar.Text>
+            </Navbar.Collapse>
+          </Container>
+        </Navbar>
+      </>
+    );
+  }
 }
 
 export default Header;
