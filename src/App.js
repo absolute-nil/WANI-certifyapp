08398fb10c4d7500ddcdb3cb50cdef9e288a839c
Nikhil Sharma <nikhil.sk.sharma@gmail.com>
2022-08-01 03:48:35 +0530

    add logged in and keycloak state to root

--- src/App.js
+++ src/App.js
@@ -2,24 +2,40 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 
 import React, { Component } from 'react';
 import { BrowserRouter, Route, Link } from 'react-router-dom';
-import Welcome from './Welcome';
-import Secured from './Secured';
+import Secured from './page/Secured';
 import './App.css';
 import { Container } from 'react-bootstrap';
 import Header from './components/header';
-import Landing from './components/landing';
-import UserInfo from './UserInfo';
+import Landing from './page/Landing';
+import UserInfo from './components/user-Info';
 
 class App extends Component {
+
+  constructor(props) {
+    super(props)
+    this.state = {
+      loggedIn: false,
+      keycloak: null
+    }
+
+    this.toggleAuthentication = this.toggleAuthentication.bind(this);
+
+  }
+
+  toggleAuthentication(keycloak) {
+    this.setState({ loggedIn: !this.state.loggedIn });
+    this.setState({ keycloak });
+
+  }
 
   render() {
     return (
       <BrowserRouter>
         <Container>
-          <Header />
+          <Header loggedIn={this.state.loggedIn} toggleAuthentication={this.toggleAuthentication} keycloak={this.state.keycloak} />
           <br />
           <Route exact path="/" component={Landing} />
-          <Route path="/secured" component={Secured} />
+          <Route path="/secured" component={Secured} toggleAuthentication={this.toggleAuthentication} />
         </Container>
       </BrowserRouter>
     );
